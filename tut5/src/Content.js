d70524b4fcf263558a26ab03d986b7077c1075bf
thatdevjustin <thatdevjustin@gmail.com>
2022-08-23 14:26:55 -0400

    tut 1-5 for react tutorial

---
+++ tut5/src/Content.js
@@ -0,0 +1,31 @@
+const Content = () => {
+    const handleNameChange = () => {
+        const names = ['Bob', 'Justin', 'Kevin'];
+        const int = Math.floor(Math.random() * 3);
+
+        return names[int];
+    }
+
+    const handleClick = () => {
+        console.log('You clicked it!')
+    }
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+    }
+    
+    return (
+        <main>
+            <p onDoubleClick={handleClick}>
+                Hello {handleNameChange()}!
+            </p>
+            <button onClick={handleClick}>Click It</button>
+            <button onClick={() => handleClick2('Justin')}>Click It</button>
+            <button onClick={(e) => handleClick3(e)}>Click It</button>
+        </main>
+    )
+}
+
+export default Content
