1b02153120eef6fd7da7ce0a5b9cda3fc94bc1cf
thatdevjustin <thatdevjustin@gmail.com>
2022-08-25 14:13:04 -0400

    finishing up tut 16 & 17

---
+++ tut16/src/NewPost.js
@@ -0,0 +1,9 @@
+const NewPost = () => {
+    return (
+        <main>
+            <h1>New Post</h1>
+        </main>
+    )
+}
+
+export default NewPost
---
+++ tut16/src/PostPage.js
@@ -0,0 +1,9 @@
+const PostPage = () => {
+    return (
+        <main>
+            <h1>Post Page</h1>
+        </main>
+    )
+}
+
+export default PostPage
